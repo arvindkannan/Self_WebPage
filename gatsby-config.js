018d5378bb3077beee70d83ecced6d9705693ef0
Arvind K <11905@ramco.com>
2019-11-03 23:05:13 +0530

    Added self blog to git

---
+++ gatsby-config.js
@@ -0,0 +1,78 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+    /* Your site config here */
+    siteMetadata: {
+        title: config.defaultTitle,
+        description: config.defaultDescription,
+        author: config.author,
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-styled-components",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-graphql",
+            options: {
+                typeName: "GitHub",
+                fieldName: "github",
+                url: "https://api.github.com/graphql",
+                headers: {
+                    Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+                },
+                fetchOptions: {},
+            },
+        },
+        {
+            resolve: "gatsby-plugin-nprogress",
+            options: {
+                color: config.themeColor,
+                showSpinner: false,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: config.googleAnalyticsID,
+                head: true,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-favicon",
+            options: {
+                logo: "./static/favicon/favicon.ico",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false,
+                },
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: config.defaultTitle,
+                short_name: "AK Blog",
+                start_url: "/",
+                background_color: config.backgroundColor,
+                theme_color: config.themeColor,
+                display: "minimal-ui",
+                icon: "./static/favicon/favicon.ico",
+            },
+        },
+        "gatsby-plugin-offline",
+    ],
+    pathPrefix: "/reponame",
+}
---
+++ src/data/config.js
@@ -0,0 +1,30 @@
+export const defaultTitle = "Arvind.K"
+export const logo = "https://portfolio.smakosh.com/favicon/favicon-512.png"
+export const author = "Arvind K"
+export const url = "https://portfolio.smakosh.com"
+export const legalName = "K Arvind"
+export const defaultDescription = "I’m Arvind and I’m a Backend Engineer!"
+export const socialLinks = {
+    github: "https://github.com/arvindkannan",
+    linkedin: "https://www.linkedin.com/in/arvindkannan1/",
+    medium: "https://medium.com/@kb_arvind",
+}
+export const googleAnalyticsID = "##"
+export const themeColor = "#6b63ff"
+export const backgroundColor = "#6b63ff"
+export const social = {
+    facebook: "appId",
+    twitter: "@arvind_kannan_",
+}
+export const address = {
+    city: "Madras",
+    region: "TN",
+    country: "India",
+    zipCode: "600088",
+}
+export const contact = {
+    email: "arvindkannan@hotmail.com",
+    phone: "phone number",
+}
+export const foundingDate = "2019"
+export const recaptcha_key = "##"
